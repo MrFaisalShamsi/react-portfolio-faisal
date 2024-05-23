a2e70d268f17b35db85f915286b36c4202c59096
Faisal <faisal181098@gmail.com>
2024-05-23 16:06:46 +0530

    add Social Sites link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = () =>{
           <h3 className="mx-2 w-10 text-5xl">Fs</h3>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl" >
-        <FaLinkedin />
-        <FaGithub />
-        <FaSquareXTwitter />
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/MDFaisalShamsi/"><FaLinkedin /></a>
+        <a href="https://github.com/MrFaisalShamsi"><FaGithub /></a>
+        {/* <FaSquareXTwitter /> */}
+        <a href="https://www.instagram.com/mr_faissu__18/#"><FaInstagram /></a>
         </div>
       </nav>
     );
