fc7200e9232b4b94d9fde4468671a7f7aa467b2a
Faisal <faisal181098@gmail.com>
2024-05-23 14:45:57 +0530

    First commit

---
+++ src/constants/index.js
@@ -0,0 +1,62 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = ` I am an enthusiastic Front-End Developer with expertise in HTML, CSS, JavaScript, Tailwind, React, and Next.js, as well as experience with Git and GitHub for collaborative development. My focus is on building responsive and user-friendly web applications that provide exceptional user experiences. I am excited to join a team where I can apply my skills, learn from experienced professionals, and contribute to innovative web solutions.`;
+
+export const ABOUT_TEXT = `With a strong background in HTML, CSS, JavaScript, Tailwind, React, and Next.js, I am a creative and detail-oriented Front-End Developer ready to take on new challenges. My familiarity with Git and GitHub for version control, along with a foundational knowledge of MySQL, allows me to contribute effectively to web development projects. I am passionate about creating seamless user experiences and am eager to bring my skills to a dynamic team where I can grow and make a meaningful impact.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2021 - Present",
+    role: "Maths Teacher",
+    company: "Meer Public School",
+    description: `Taught mathematics to students in grades 9-10, covering topics such as Algebra, Geometry, and Pre-Calculus.Participated in ongoing professional development workshops and training sessions to stay current with educational best practices and emerging technologies.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2021 - Present",
+    role: "Computer Teacher",
+    company: "Meer Public School",
+    description: `Taught computer science and technology courses to students in grades 6-12, covering topics such as programming (C, C++, JavaScript), web development (HTML, CSS), and basic computer operations.Created comprehensive instructional resources, including coding exercises, project guidelines, and multimedia presentations to support diverse learning styles.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Sundown Studio Website",
+    image: project1,
+    description:
+      "A landing page of a website with features like animation, well-designation, and user authentication.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Shoe Brand Page",
+    image: project2,
+    description:
+      "A webpage for showcasing the product and shopping cart and listing categories like features.",
+    technologies: ["HTML", "CSS", "Javascript", "React"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind"],
+  },
+  {
+    title: "Keep Notes",
+    image: project4,
+    description:
+      "A Webpage for creating and publishing notes, with features like rich text editing and CRUD operation.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+];
+
+export const CONTACT = {
+  address: "Shamli, Uttar Pradesh, India ",
+  phoneNo: "+91 7817052432  ",
+  email: "faisal181098@gmail.com",
+};
